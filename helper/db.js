76af8f84556d1acccdff1c64dd860c3f462f9d7b
melihozden <melihozden403@gmail.com>
2018-10-23 01:12:29 +0300

    Update db.js

--- helper/db.js
+++ helper/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 
 module.exports = () =>{
-    mongoose.connect("mongodb://melihozden:12345abc@ds233763.mlab.com:33763/movie-api",{useNewUrlParser:true})
+    mongoose.connect("mongodb://<username>:<password>@ds233763.mlab.com:33763/movie-api",{useNewUrlParser:true})
 
 mongoose.connection.on('open',()=>{
     console.log("Mongodb status : On")
